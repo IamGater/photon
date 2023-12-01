2c0bdf5263aa8841a50828c88f4679ad9ca868d5
IamGater <116513307+IamGater@users.noreply.github.com>
2023-12-01 12:54:41 +0200

    search clearer and some css

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ let searchValue;
 
 searchInput.addEventListener("input", updateInput);
 form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
 });
+more.addEventListener("click", loadMore);
 
 function updateInput(e) {
-  e.preventDefault();
   searchValue = e.target.value;
 }
 
@@ -29,8 +31,12 @@ function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+    galleryImg.innerHTML = `
+      <div class="gallery-info">
         <p>${photo.photographer}</p>
+        <a href=${photo.src.original}>Download</a>
+      </div>
+      <img src=${photo.src.large}></img>
         `;
     gallery.appendChild(galleryImg);
   });
@@ -44,10 +50,16 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(query) {
+  clear();
   const data = await fetchApi(
     `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
   );
   generatePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
 }
 
 curatedPhotos();
